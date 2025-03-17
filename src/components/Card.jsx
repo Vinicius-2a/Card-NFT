fa36127f6c09a6932e4ab5791449251dd8e21925
Vinicius Almeida <Vinicius.almeida.ayres@gmail.com>
2025-03-17 10:02:47 -0300

    corrigindo erro das imagens

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,19 @@
 import styles from './Card.module.css';
+import image_equilibrium from '../assets/image-equilibrium.jpg'
+import image_overlay from '../assets/icon-view.svg'
+import image_ETH from '../assets/icon-ethereum.svg'
+import image_DATE from '../assets/icon-ethereum.svg'
+import image_Author from '../assets/image-avatar.jfif'
+
 
 export function Card() {
   return (
     <main className={styles.card}>
       <article>
         <header className={styles.card_image_wrapper}>
-          <img className={styles.card_image} src="/assets/image-equilibrium.jpg" alt="Equilibrium #3429" />
+        <img className={styles.card_image} src={image_equilibrium} />
           <div className={styles.card_overlay}>
-            <img src="/assets/icon-view.svg" alt="view icon" />
+            <img src={image_overlay} alt="view icon" />
           </div>
         </header>
 
@@ -17,18 +23,18 @@ export function Card() {
 
           <div className={styles.card_info}>
             <div className={`${styles.card_info_item} ${styles.info_ETH}`}>
-              <img src="/assets/icon-ethereum.svg" alt="ethereum" />
+              <img src={image_ETH} alt="ethereum" />
               <p>0.041 ETH</p>
             </div>
             <div className={`${styles.card_info_item} ${styles.info_DATE}`}>
-              <img src="/assets/icon-clock.svg" alt="clock" />
+              <img src={image_DATE} alt="clock" />
               <p>Faltam 3 dias</p>
             </div>
           </div>
         </section>
 
         <footer className={styles.card_author}>
-          <img src="/assets/image-avatar.jfif" alt="author avatar" />
+          <img src={image_Author} alt="author avatar" />
           <p>Criação de <span>Vinicius-2a</span></p>
         </footer>
       </article>
